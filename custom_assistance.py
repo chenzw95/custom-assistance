ef29c43cb0dce2393e64250e0236625243ef8416
chenzw <chenzw25@gmail.com>
2017-05-20 17:01:23 +0800

    updates for b9s, remove updateadv command

--- custom_assistance.py
+++ custom_assistance.py
@@ -18,28 +18,20 @@ class Assistance:
     async def lumacfg(self, ctx):
         """Lists options to enable in Luma3DS configuration."""
         await self.bot.delete_message(ctx.message)
-        await self.simple_embed("• Autoboot SysNAND\n• Use SysNAND FIRM if booting with R\n• Show NAND or user string in System Settings\n\nIf you are configuring Luma3DS while the SD card is **not inserted** (CTRNAND Luma3DS), it is normal if you cannot see some of the above options.", title="Options to enable in Luma3DS configuration")
-
-    @commands.command(pass_context=True)
-    async def updateadv(self, ctx):
-        """Firmware update advisory (generic)"""
-        await self.bot.delete_message(ctx.message)
-        embed = discord.Embed(title="Is it safe to update?", color=discord.Color.blue())
-        embed.description = "In general, it is safe to update if you have installed A9LH/CFW."
-        embed.add_field(name="With CFW", value="Firmware updates with A9LH/CFW are safe. Before performing the system update, first update to the latest normal version of Luma3DS, either via the Luma Updater or by [manually downloading Luma3DS](https://github.com/AuroraWright/Luma3DS/releases) and replacing arm9loaderhax.bin on the SD card.\nYou should also repeat [section IX of the guide's last page](https://3ds.guide/installing-arm9loaderhax#section-ix---ctrnand-luma3ds) to ensure SD-less boot will work properly.")
-        embed.add_field(name="Without CFW (stock firmware)", value="You are advised to consider installing CFW. For now, 11.3 is the latest firmware version where CFW installation is possible. soundhax has been patched in 11.4, and the Homebrew Launcher does not work on the O3DS at all.")
-        embed.add_field(name="If you do not want to update", value="You can run [the latest version of ctr-httpwn](https://github.com/yellows8/ctr-httpwn/releases) from the Homebrew Launcher to bypass most update requirements.")
+        embed = discord.Embed(title="Options to enable in Luma3DS configuration")
+        embed.add_field(name="boot9strap", value="• Show NAND or user string in System Settings")
+        embed.add_field(name="arm9loaderhax (deprecated)", value="• Autoboot SysNAND\n• Use SysNAND FIRM if booting with R\n• Show NAND or user string in System Settings\n\nIf you are configuring Luma3DS while the SD card is **not inserted** (CTRNAND Luma3DS), it is normal if you cannot see some of the above options.")
         await self.bot.say("", embed=embed)
 
     @commands.command(pass_context=True)
     async def updatecfw(self, ctx):
-        """Firmware update advisory (A9LH)"""
+        """Firmware update advisory (CFW)"""
         await self.bot.delete_message(ctx.message)
-        embed = discord.Embed(title="Is it safe to update if I have installed A9LH/CFW already?", color=discord.Color.blue())
-        embed.description = "In general, it is safe to update if you have installed A9LH/CFW. Note, however, that ctr-httpwn has been patched in 11.4."
-        embed.add_field(name="O3DS/2DS", value="If you still need the Homebrew Launcher, refer to the pinned messages in <#196635695958196224> and copy the payload file for your console to the `hblauncherloader` folder.")
-        embed.add_field(name="N3DS", value="There are no issues with updating to 11.4.")
-        embed.add_field(name="How to update", value="Update to the latest normal version of Luma3DS first, either via the Luma Updater or by [manually downloading Luma3DS](https://github.com/AuroraWright/Luma3DS/releases) and replacing arm9loaderhax.bin on the SD card.\nYou should also repeat [section IX of the guide's last page](https://3ds.guide/installing-arm9loaderhax#section-ix---ctrnand-luma3ds) to ensure SD-less boot will work properly.")
+        embed = discord.Embed(title="Is it safe to update if I have installed CFW already?", color=discord.Color.blue())
+        embed.description = "In general, it is safe to update if you have installed CFW. Note, however, that ctr-httpwn has been patched in 11.4."
+        embed.add_field(name="boot9strap", value="Update to the latest normal version of Luma3DS first, either via the Luma Updater or by [manually downloading Luma3DS](https://github.com/AuroraWright/Luma3DS/releases) and replacing `boot.firm` on the SD card.\nYou should also repeat [section VIII of the guide's last page](https://3ds.guide/finalizing-setup#section-viii---ctrnand-luma3ds) to ensure SD-less boot will work properly.")
+        embed.add_field(name="arm9loaderhax (deprecated)", value="You should [update to boot9strap](https://3ds.guide/updating-to-boot9strap) first.")
+        embed.add_field(name="Additional notes", value="If you still need the Homebrew Launcher on O3DS/2DS, refer to the pinned messages in <#196635695958196224> and copy the payload file for your console to the `hblauncherloader` folder.")
         await self.bot.say("", embed=embed)
 
     @commands.command(pass_context=True)
@@ -54,7 +46,8 @@ class Assistance:
         """Steps to repeat if SD card has been formatted"""
         await self.bot.delete_message(ctx.message)
         embed = discord.Embed(title="Blank SD card?", color=discord.Color.red())
-        embed.description = "If you have installed CFW and formatted your SD card, repeat sections I, III, V, VI and VIII of [Installing arm9loaderhax](https://3ds.guide/installing-arm9loaderhax)."
+        embed.add_field(name="boot9strap", value="Repeat the steps listed on [Finalizing setup](https://3ds.guide/finalizing-setup).")
+        embed.add_field(name="arm9loaderhax (deprecated)", value="Repeat the steps listed on [Finalizing setup](https://3ds.guide/finalizing-setup), but download [Luma3DS v7.0.5](https://github.com/AuroraWright/Luma3DS/releases/tag/v7.0.5) instead of the latest release. When prompted to copy `boot.firm`, copy `arm9loaderhax.bin` instead.\n**You are advised to [update to boot9strap](https://3ds.guide/updating-to-boot9strap).**")
         await self.bot.say("", embed=embed)
 
 
