7a43a2a4a3ee709020060736ef8e675682c0f16a
chenzw <chenzw25@gmail.com>
2017-05-22 22:27:08 +0800

    add stock114 command, with more detailed explanation

--- custom_assistance.py
+++ custom_assistance.py
@@ -61,5 +61,16 @@ class Assistance:
         embed.add_field(name="How do I install B9S from stock?", value="Follow [the guide](https://3ds.guide).")
         await self.bot.say("", embed=embed)
 
+    @commands.command(pass_context=True)
+    async def stock114(self, ctx):
+        """Advisory for users on stock 11.4 firmware"""
+        await self.bot.delete_message(ctx.message)
+        embed = discord.Embed(title="Regarding users on stock 11.4 firmware...", color=discord.Color.blue())
+        embed.description = "To install CFW on stock 11.4 firmware, the only options available right now are to use a [DSiWare exploit](https://3ds.guide/installing-boot9strap-(dsiware\)) or by [hardmodding the console](https://3ds.guide/installing-boot9strap-(hardmod\))."
+        embed.add_field(name="But these methods are so difficult!", value="You will have to wait for a kernel exploit that works on 11.4 firmware. As ordinary helpers, we cannot tell you when such an exploit will be available.")
+        embed.add_field(name="Can you inform me when such an exploit is available?", value="You should either refer to <#225556031428755456> or [/r/3dshacks](https://www.reddit.com/r/3dshacks).")
+        await self.bot.say("", embed=embed)
+
+
 def setup(bot):
     bot.add_cog(Assistance(bot))
