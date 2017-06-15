10022279afa1cd80083037fe80e5154a3df4158f
GriffinG1 <griffin.giargiari@gmail.com>
2017-06-14 19:57:34 -0700

    Update custom_assistance.py

--- custom_assistance.py
+++ custom_assistance.py
@@ -19,8 +19,9 @@ class Assistance:
         """Lists information on the module and links to the module"""
         await self.bot.delete_message(ctx.message)
         embed = discord.Embed(title="Chenzw's custom assistance module")
-        embed.add_field(name="Where can I download this?", value="https://github.com/chenzw95/custom-assistance")
+        embed.description = "Thanks for the interest in this module!"
         embed.add_field(name="What is this for?", value="This module is to help with 3DS Hacking related assistance, primarily for the Nintendo Homebrew server.")
+        embed.add_field(name="Where can I download this?", value="https://github.com/chenzw95/custom-assistance")
         await self.bot.say("", embed=embed)
     
     @commands.command(pass_context=True)
